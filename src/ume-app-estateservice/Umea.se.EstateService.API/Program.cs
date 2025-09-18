3bde51df6c33f25ac1149f3858e262bc2a001892
ume-nichlas <nichlas.lofdahl@umea.se>
2025-09-18 16:15:23 +0200

    Set customer pythagoras header

--- src/ume-app-estateservice/Umea.se.EstateService.API/Program.cs
+++ src/ume-app-estateservice/Umea.se.EstateService.API/Program.cs
@@ -30,6 +30,8 @@ builder.Services.AddDefaultHttpClient(HttpClientNames.Pythagoras, options =>
 {
     options.BaseAddress = config.PythagorasBaseUrl;
     options.XApiKey = config.PythagorasApiKey;
+    // The below works when improvement/14157 is merged and released
+    //options.DefaultRequestHeaders.Add("api_key", config.PythagorasApiKey);
 });
 
 builder.Services.AddAuthentication(options =>
