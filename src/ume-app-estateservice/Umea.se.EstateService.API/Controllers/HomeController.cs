e7401215804043c1da5a76d7f9df9e8f2c8573c4
Nichlas Löfdahl <nichlas.lofdahl@umea.se>
2025-09-23 10:41:35 +0200

    Change ping route (#10)

--- src/ume-app-estateservice/Umea.se.EstateService.API/Controllers/HomeController.cs
+++ src/ume-app-estateservice/Umea.se.EstateService.API/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using Umea.se.Toolkit.Configuration;
+﻿using Microsoft.AspNetCore.Mvc;
+using Umea.se.Toolkit.Configuration;
 using Umea.se.Toolkit.Controllers;
 
 namespace Umea.se.EstateService.API.Controllers;
 
+
+[ApiController]
+[Route(ApiRoutesBase.Home)]
 public class HomeController(ILogger<HomeController> logger, ApplicationConfigBase config) : HomeControllerBase(logger, config)
 {
 }
