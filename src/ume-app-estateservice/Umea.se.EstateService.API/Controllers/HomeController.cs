4eb0fa49cde12fc5deeb4b0c2e4ae95a75ba43ec
ume-nichlas <nichlas.lofdahl@umea.se>
2025-09-23 10:13:44 +0200

    Use HomeControllerBase

--- src/ume-app-estateservice/Umea.se.EstateService.API/Controllers/HomeController.cs
+++ src/ume-app-estateservice/Umea.se.EstateService.API/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Umea.se.Toolkit.Configuration;
+using Umea.se.Toolkit.Controllers;
 
 namespace Umea.se.EstateService.API.Controllers;
 
-public class HomeController : Controller
+public class HomeController(ILogger<HomeController> logger, ApplicationConfigBase config) : HomeControllerBase(logger, config)
 {
-    public IActionResult Index()
-    {
-        return View();
-    }
 }
