f0c7f576b54a07cc54ea2944edadf8a8e810eed9
Kim Vallén <36292640+thekim1@users.noreply.github.com>
2025-09-23 11:03:44 +0200

    Downgrade Swashbuckle and update NuGet config (#11)
    Downgraded Swashbuckle.AspNetCore to version 8.1.4 in `Directory.Packages.props`.
    Added `<packageSourceMapping>` to `NuGet.Config` for better package source control.
    Modified `HomeController` by removing `[ApiController]` and route attributes.
    Updated Swagger configuration in `Program.cs` to use custom schema IDs.
    Included `Directory.Packages.props` and `NuGet.Config` in the solution file.
    Performed minor formatting and cleanup in XML files.

--- src/ume-app-estateservice/Umea.se.EstateService.API/Controllers/HomeController.cs
+++ src/ume-app-estateservice/Umea.se.EstateService.API/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using Umea.se.Toolkit.Controllers;
 
 namespace Umea.se.EstateService.API.Controllers;
 
-
 [ApiController]
 [Route(ApiRoutesBase.Home)]
 public class HomeController(ILogger<HomeController> logger, ApplicationConfigBase config) : HomeControllerBase(logger, config)
