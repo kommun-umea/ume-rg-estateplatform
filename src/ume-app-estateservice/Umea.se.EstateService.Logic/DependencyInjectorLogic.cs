e12c816ef558e72b34bc902336cf3773b6881232
ume-nichlas <nichlas.lofdahl@umea.se>
2025-09-23 10:13:57 +0200

    Add IPythagorasHandler to dependency injection

--- src/ume-app-estateservice/Umea.se.EstateService.Logic/DependencyInjectorLogic.cs
+++ src/ume-app-estateservice/Umea.se.EstateService.Logic/DependencyInjectorLogic.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Umea.se.EstateService.Logic.Handlers;
+using Umea.se.EstateService.Logic.Interfaces;
 
 namespace Umea.se.EstateService.Logic;
 
@@ -6,6 +8,7 @@ public static class DependencyInjectorLogic
 {
     public static IServiceCollection AddLogicDependencies(this IServiceCollection services)
     {
+        services.AddSingleton<IPythagorasHandler, PythagorasHandler>();
         return services;
     }
 }
