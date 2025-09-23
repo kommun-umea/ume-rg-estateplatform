95b9857aa9f0b5448269e49874efa41594900525
ume-nichlas <nichlas.lofdahl@umea.se>
2025-09-23 09:59:38 +0200

    Restore HomeController

---
+++ src/ume-app-estateservice/Umea.se.EstateService.API/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Umea.se.EstateService.API.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
